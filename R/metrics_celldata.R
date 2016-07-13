a9bfbafec855237713894e4489470ff543229a72
S-Holzhauer <S-Holzhauer@localhost>
2016-07-14 00:57:33 +0100

    :: craftyr :: 0.3.6 | metrics - set version 0.3.6 - added forgotten metrics_agg.R - removed feature: metric_regional_diversity

--- R/metrics_celldata.R
+++ R/metrics_celldata.R
@@ -27,64 +27,9 @@ metric_celldata_changes <- function(simp, cdata, ncol = 1, title = "AFT-Changes"
 	}
 }
 
-#' TODO to complete
-#' TODO think about un-regular regions (real-world) (use shapefiles?)
-#' 
-#' @param simp 
-#' @param data 
-#' @param regionwidth 
-#' @param regionheight 
-#' @return regional diversity metric
-#' 
-#' @author Calum Brown
-#' @author Sascha Holzhauer
-#' 
-#' @export
-metric_regional_diversity <- function(simp, data, regionwidth = 50, regionheight = regionwidth) {
-
-	min_x <- min(data[simp$csv$cname_x])
-	max_x <- max(data[simp$csv$cname_x])
-	
-	min_y <- min(data[simp$csv$cname_y])
-	max_y <- max(data[simp$csv$cname_y])
-	
-	
-	xlims <- seq(from = min_x, to=max_x, by=regionwidth)
-	xlims <- seq(from = min_y, to=max_y, by=regionheight)
-	
-	diversity<-rep(0, length(xlims) * length(ylims))
-	
-	k <- 0
-	for(i in c(1:length(xlims))) {
-		for (j in c(1:length(ylims))) {
-			k <- k+1
-			
-			region<-subset(data,data$X>xlims[i] & data$X<=xlims[i+1] & data$Y>ylims[j] & data$Y<=ylims[j+1])
-			
-			for (aft in simp$sim$AftNames) {
-				# TODO
-			}
-			NCCereal<-length(region$Agent[region$Agent=="NC_Cereal"])
-			CCereal<-length(region$Agent[region$Agent=="C_Cereal"])
-			NCLivestock<-length(region$Agent[region$Agent=="NC_Livestock"])
-			CLivestock<-length(region$Agent[region$Agent=="C_Livestock"])
-			Forester<-length(region$Agent[region$Agent=="Forester"])
-			Conservationist<-length(region$Agent[region$Agent=="Conservationist"])
-			Biofuel<-length(region$Agent[region$Agent=="BiofuelFarmer"])
-			tot<-length(region$Agent)
-			
-			region.Sdiv<-1-(((NCCereal/tot)^2)+((CCereal/tot)^2)+((NCLivestock/tot)^2)+((CLivestock/tot)^2)+
-						((Forester/tot)^2)+((Conservationist/tot)^2)+((Biofuel/tot)^2))     
-			diversity[k]<-region.Sdiv
-		}
-	}
-	result<-mean(diversity)
-	return(result)
-}
-
 # Functions
 # TODO
-metric_global_connectivity <- function(sip, data, type) {
+metric_celldata_connectivity_global <- function(sip, data, type) {
 	
 	
 	datatemp<-data
